46de82c068198158fb440a5117368ce6d7fb9fbe
william.liao <william.liao@mitake.com.tw>
2021-05-07 15:09:32 +0800

    update mockURLSession

--- UnsplashWallpapers/Service/NetworkManager+Mock.swift
+++ UnsplashWallpapers/Service/NetworkManager+Mock.swift
@@ -7,13 +7,15 @@
 
 import Foundation
 
+
+
 class MockURLSessionDataTask: URLSessionDataTaskProtocol {
     func cancel() {}
     func resume() {}
 }
 
 class MockURLSession: URLSessionProtocol {
-    
+    private (set) var lastURL: URL?
     var dataTask = MockURLSessionDataTask()
     var completionHandler: (Data?, URLResponse?, Error?)
 
@@ -29,5 +31,46 @@ class MockURLSession: URLSessionProtocol {
     func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
         completionHandler(self.completionHandler.0, self.completionHandler.1, self.completionHandler.2)
         return dataTask
+    }
+    
+    func dataTaskWithURL(_ url: URL, completion: @escaping DataTaskResult) -> URLSessionDataTask {
+        lastURL = url
+        return URLSession.shared.dataTask(with: url)
+    }
+}
+
+// We create a partial mock by subclassing the original class
+class URLSessionDataTaskMock: URLSessionDataTask {
+    private let closure: () -> Void
+
+    init(closure: @escaping () -> Void) {
+        self.closure = closure
+    }
+
+    // We override the 'resume' method and simply call our closure
+    // instead of actually resuming any task.
+    override func resume() {
+        closure()
+    }
+}
+
+class URLSessionMock: URLSession {
+    typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
+
+    // Properties that enable us to set exactly what data or error
+    // we want our mocked URLSession to return for any request.
+    var data: Data?
+    var error: Error?
+
+    override func dataTask(
+        with url: URL,
+        completionHandler: @escaping CompletionHandler
+    ) -> URLSessionDataTask {
+        let data = self.data
+        let error = self.error
+
+        return URLSessionDataTaskMock {
+            completionHandler(data, nil, error)
+        }
     }
 }
